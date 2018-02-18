4108efb80839a146e1b235b00831ff6fe96f8cae
nao20010128nao <nao20010128@gmail.com>
2018-02-19 00:59:32 +0900

    追加

--- processor/src/main/java/com/nao20010128nao/bindingviewholder/processor/Processor.kt
+++ processor/src/main/java/com/nao20010128nao/bindingviewholder/processor/Processor.kt
@@ -17,8 +17,11 @@ class Processor : AbstractProcessor() {
         types!!
         val dataBinding = processingEnv!!.elementUtils!!.getPackageElement("android.databinding.ViewDataBinding")!!
         types.flatMap { round.getElementsAnnotatedWith(it) }
+                .also { it.forEach { processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, "1", it) } }
                 .filter { it.kind == ElementKind.CLASS }
+                .also { it.forEach { processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, "2", it) } }
                 .map { it as TypeElement }
+                .also { it.forEach { processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, "3", it) } }
                 .filter { it.superclass == dataBinding || it.toString() == dataBinding.toString() }
                 .forEach { processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, "Found", it) }
         return true
